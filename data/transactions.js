62369f57f042e41262644dec2a5e14397753d5ce
Hafizur Rahman <hafizur@Hafizurs-MBP.lan>
2022-08-31 21:53:57 -0400

    Added data folder and pyaers videos todos tags and transactions json files

---
+++ data/transactions.js
@@ -0,0 +1,14 @@
+[
+    {
+        "id": 1,
+        "name": "Employment Salary",
+        "type": "income",
+        "amount": 1000000
+    },
+    {
+        "id": 2,
+        "name": "Family Expenses",
+        "type": "expense",
+        "amount": 200000
+    }
+]
